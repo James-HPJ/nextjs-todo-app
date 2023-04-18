c66563c4db5c90cbc2cdc1f3f6c20b55a45eb6e8
James-HPJ <85088804+James-HPJ@users.noreply.github.com>
2023-04-18 22:19:49 +0800

    ready for deployment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import Layout from "@/components/layout/Layout";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,139 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useCallback, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
+import NoteForm from "@/components/form/NoteForm";
+import NotesGrid from "@/components/notes-list/NotesGrid";
+import ShowNotification from "@/components/UI/ShowNotification";
 
-const inter = Inter({ subsets: ['latin'] })
+const HomePage = () => {
+  const router = useRouter();
+  const [notes, setNotes] = useState([]);
+  const [arrangeNotes, setArrangeNotes] = useState("desc");
+  const [showStatusMessage, setShowStatusMessage] = useState(null);
 
-export default function Home() {
+  const fetchNotes = useCallback(async (order) => {
+    try {
+      const response = await fetch("/api/notes/?sort=" + order);
+
+      if (!response.ok) {
+        setShowStatusMessage({
+          message:
+            `Error: ${response.message}` ||
+            "Error: Could not retrieve your tasks at the present moment",
+          isError: true,
+        });
+        return;
+      }
+
+      const data = await response.json();
+      setNotes(data.notes);
+    } catch (error) {
+      setShowStatusMessage({
+        message:
+          `Error: ${error.message}` ||
+          "Error: Could not retrieve your tasks at the present moment",
+        isError: true,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchNotes(arrangeNotes);
+  }, [fetchNotes, arrangeNotes, showStatusMessage]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowStatusMessage(null);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [showStatusMessage]);
+
+  const addNote = (newNote) => {
+    fetch("/api/notes", {
+      method: "POST",
+      body: JSON.stringify({
+        title: newNote.title,
+        content: newNote.content,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          setShowStatusMessage({
+            message:
+              "Error: " + response.message ||
+              "Error: We could not create your task",
+            isError: true,
+          });
+          return;
+        }
+
+        setShowStatusMessage({
+          message: "Task created and added!",
+          isError: false,
+        });
+      })
+
+      .catch((error) => {
+        setShowStatusMessage({
+          message:
+            "Error: " + error.message || "Error: We could not create your task",
+          isError: true,
+        });
+      });
+
+    fetchNotes(arrangeNotes);
+  };
+
+  const arrangeNotesHandler = () => {
+    setArrangeNotes((state) => (state === "desc" ? "asc" : "desc"));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <NoteForm onSubmit={addNote} item={{ title: "", content: "" }} />
+      {notes && notes.length > 0 && (
+        <NotesGrid
+          notes={notes}
+          order={arrangeNotes}
+          onSubmit={arrangeNotesHandler}
         />
-      </div>
+      )}
+      {showStatusMessage && (
+        <ShowNotification
+          message={showStatusMessage.message}
+          isError={showStatusMessage.isError}
+        />
+      )}
+    </>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export const getServerSideProps = async (context) => {
+  const sess = await getServerSession(context.req, context.res, authOptions);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  if (!sess) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+  const { query } = context;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+  return {
+    props: {
+      searchQuery: query.search || "",
+    },
+  };
+};
+
+export default HomePage;
